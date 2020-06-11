6f9bc6149474a6addbe00a41eadf9762df9a5019
gasparsegura <64159600+gasparsegura@users.noreply.github.com>
2020-06-11 01:05:54 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 #include <string.h>
 #include <fstream>
 #include <sstream>
-#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+
 
 using namespace std;
 
@@ -129,7 +132,268 @@ public:
 };
 
 
+/////////////////////////////////////////////Codigo de Prueba////////////////////////////////////////////////
 
+template <class T> class NodoBin {
+public:
+    NodoBin(){};
+    Evento evento;
+    T info, grado;
+    NodoBin *p;
+    NodoBin *hijo;
+    NodoBin *hermano;
+};
+
+
+template <class T> class BinomialHeap {
+private:
+    NodoBin<T>* raiz;				// apunta a la raíz del árbol binomial más a la izquierda
+
+    void inicioNodo(NodoBin<T>* nodo, T info, T grado, Evento event) {		//inicializo en null
+        nodo->evento = event;
+        nodo->info = info;
+        nodo->grado = grado;
+        nodo->p = NULL;
+        nodo->hijo = NULL;
+        nodo->hermano = NULL;
+    }
+
+    static void UnirArbolBinomial(NodoBin<T>* x, NodoBin<T>* y) {
+        // x debe ser padre de y
+        y->p = x;
+        y->hermano = x->hijo;
+        x->hijo = y;
+
+        // aumenta el grado de x
+        x->grado += 1;
+    }
+
+public:
+    BinomialHeap() {	raiz = NULL;	}
+
+
+/*	NodePtr getMinimo() {							// encuentra el nodo con los datos mas chicos (compara todas las raices de los arboles y elije la mas chica)
+		int min = 999999;
+		NodePtr raizPtr = raiz;
+		NodePtr minPtr = NULL;
+
+		while (raizPtr != NULL) {
+			if (raizPtr->info < min) {
+				min = raizPtr->info;
+				minPtr = raizPtr;
+			}
+			raizPtr = raizPtr->hermano;
+		}
+
+		return minPtr;
+
+	}*/
+
+
+    void creoHeap() {
+        // B0
+        Evento* ev = new Evento("Evento original",0);
+        NodoBin<T>* node1 = new NodoBin<T>;
+        inicioNodo(node1, 0, 0, *ev);
+        raiz = node1;
+
+    }
+
+
+    void insert(int info, Evento ev) {        //inserto un nodo
+        BinomialHeap h;
+        NodoBin<T>* node = new NodoBin<T>;
+        inicioNodo(node, info, 0, ev);
+        h.setRaiz(node);
+        merge(h);
+    }
+
+/*
+    void printHeap() {
+        Nodo<T>* currPtr = raiz;
+        while (currPtr != NULL) {
+            cout<<"B"<<currPtr->grado<<endl;
+            cout<<"Hay "<<pow(2, currPtr->grado)<<" nodos en este arbol"<<endl;
+            cout<<"El recorrido de orden de nivel es"<<endl;
+            queue<Nodo<T>*> q;
+            q.push(currPtr);
+            while (!q.empty()) {
+                Nodo<T>* p = q.front();
+                q.pop();
+                cout<<p->info<<" ";
+
+                if (p->hijo != NULL) {
+                    Nodo<T>* tempPtr = p->hijo;
+                    while (tempPtr != NULL) {
+                        q.push(tempPtr);
+                        tempPtr = tempPtr->hermano;
+                    }
+                }
+            }
+            currPtr = currPtr->hermano;
+            cout<<endl<<endl;
+        }
+    }
+
+*/
+    NodoBin<T>* getRaiz() {
+        return raiz;
+    }
+
+    void setRaiz(NodoBin<T>* raiz) {
+        this->raiz = raiz;
+    }
+
+
+    //fusiona dos heap binomial del mismo grado H y H' = H
+    void merge(BinomialHeap h1) {
+        NodoBin<T>* curr1 = getRaiz();
+        NodoBin<T>* curr2 = h1.getRaiz();
+        NodoBin<T>* curr3 = NULL;
+        NodoBin<T>* temp = NULL;
+
+        if (curr1->grado <= curr2->grado) {
+            curr3 = curr1;
+            curr1 = curr1->hermano;
+        } else {
+            curr3 = curr2;
+            curr2 = curr2->hermano;
+        }
+
+        temp = curr3;
+
+        // 1- fusiona dos montones sin cuidar los árboles con el mismo grado
+        //    las raíces del árbol deben estar en orden ascendente de grado de izquierda a derecha
+        while(curr1 != NULL && curr2 != NULL) {
+            if (curr1->grado <= curr2->grado) {
+                curr3->hermano = curr1;
+                curr1 = curr1->hermano;
+            } else {
+                curr3->hermano = curr2;
+                curr2 = curr2->hermano;
+            }
+            curr3 = curr3->hermano;
+        }
+
+        // copia todos los árboles restantes de heap1
+        if (curr1 != NULL) {
+            while(curr1 != NULL) {
+                curr3->hermano = curr1;
+                curr1 = curr1->hermano;
+                curr3 = curr3->hermano;
+            }
+        }
+
+        // copia todos los árboles restantes de heap2
+        if (curr2 != NULL) {
+            while(curr2 != NULL) {
+                curr3->hermano = curr2;
+                curr2 = curr2->hermano;
+                curr3 = curr3->hermano;
+            }
+        }
+
+        // 2- combina repetidamente árboles binarios con el mismo grado
+        curr3 = temp;
+        NodoBin<T>* prev = NULL;
+        NodoBin<T>* next = curr3->hermano;
+
+        while (next != NULL) {
+            // si dos raíces de árboles adyacentes tienen un grado diferente o 3 raíces consecutivas tienen el mismo grado
+            // pasar al siguiente árbol
+            if ((curr3->grado != next->grado )|| (next->hermano != NULL && curr3->grado == next->hermano->grado)) {
+                prev = curr3;
+                curr3 = next;
+            } else {
+                // de lo contrario, fusionar repetidamente árboles binomiales con el mismo grado (tiene que identif la menor raiz)
+                if (curr3->info <= next->info) {
+                    curr3->hermano = next->hermano;
+                    BinomialHeap::UnirArbolBinomial(curr3, next);
+                } else {
+                    if (prev == NULL) {
+                        temp = next;
+                    } else {
+                        prev->hermano = next;
+                    }
+
+                    BinomialHeap::UnirArbolBinomial(next, curr3);
+                    curr3 = next;
+                }
+            }
+
+            next = curr3->hermano;
+        }
+
+        setRaiz(temp);
+    }
+
+
+    NodoBin<T>* borrarMinimo() {
+
+        NodoBin<T>* curr = raiz;
+        NodoBin<T>* prevMin = NULL;
+        // encuentra la raiz con el dato minimo
+        NodoBin<T>* minPtr = NULL;
+        NodoBin<T>* prevPtr = NULL;
+        int min = 999999;
+
+        while (curr != NULL) {
+            if (curr->info <= min) {
+                min = curr->info;
+                prevMin = prevPtr;
+                minPtr = curr;
+            }
+            prevPtr = curr;
+            curr = curr->hermano;
+        }
+
+        // borro el puntero al nodo minimo
+        if (prevMin != NULL && minPtr->hermano != NULL) {
+            prevMin->hermano = minPtr->hermano;
+        } else if (prevMin != NULL && minPtr->hermano == NULL) {
+            prevMin->hermano = NULL;
+        }
+
+        // borraro el puntero a padre
+        NodoBin<T>* childPtr = minPtr->hijo;
+        while (childPtr != NULL) {
+            childPtr->p = NULL;
+            childPtr = childPtr->hermano;
+        }
+
+        // reacomodo
+        stack<NodoBin<T>*> s;
+        childPtr = minPtr->hijo;
+        while (childPtr != NULL) {
+            s.push(childPtr);
+            childPtr = childPtr->hermano;
+        }
+
+        curr = s.top();
+        NodoBin<T>* temp = curr;
+        s.pop();
+
+        while (!s.empty()) {
+            curr->hermano = s.top();
+            s.pop();
+            curr = curr->hermano;
+        }
+
+        curr->hermano = NULL;
+
+        BinomialHeap h;
+        h.setRaiz(temp);
+
+        merge(h);
+
+        return minPtr;
+    }
+
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //-------------------------------------------------------------------------------------------
 //  RELOJ
@@ -346,6 +610,7 @@ void Planificador1::ordenaQS(int primero, int ultimo){
         }
         swapEventos(i, ultimo);
 
+        // Recursion Binaria
         ordenaQS(primero,i-1);
         ordenaQS(i+1,ultimo);
     }
@@ -570,6 +835,46 @@ Evento Planificador2::getProximoEvento() {
 }
 
 
+
+//-------------------------------------------------------------------------------------------
+//  PLANIFICADOR 3 (Heap Binomial)
+
+class Planificador3 : public Planificador{
+
+private:
+    BinomialHeap<int> binHeap;
+
+public:
+    Planificador3(){binHeap.creoHeap();};
+    void agregarEventos() override;
+    Evento getProximoEvento() override;
+};
+
+void Planificador3::agregarEventos() {
+
+    // Se inicializa nodoReloj con el puntero al comienzo de la lista
+    Nodo<Reloj*>* nodoReloj = relojes->nodo();
+
+    // Se recorre la lista de relojes y se le extraen los eventos a cada uno
+    while(!nodoReloj->es_vacio()){
+
+        // Arreglo de punteros a objetos Evento
+        Evento** temp = nodoReloj->get_dato()->getEventos();
+
+        for(int x = 0; x < 50; x++){
+
+            binHeap.insert(temp[x]->horaDeEjecucion,*temp[x]);
+        }
+        nodoReloj = nodoReloj->get_next();
+    }
+}
+
+Evento Planificador3::getProximoEvento() {
+
+    return binHeap.borrarMinimo()->evento;
+
+}
+
 //-------------------------------------------------------------------------------------------
 //  Funciones
 
@@ -617,6 +922,7 @@ int main(){
     // Inicializar planificadores
     Planificador1* p1 = new Planificador1();
     Planificador2* p2 = new Planificador2();
+    Planificador3* p3 = new Planificador3();
 
     if(archivo.is_open()){
 
@@ -629,8 +935,13 @@ int main(){
 
         cargarRelojes(p2, archivo);
 
-        archivo.close();
+        archivo.clear();
+        archivo.seekg(0, std::ios::beg);
 
+        cargarRelojes(p3, archivo);
+
+        archivo.close();
+/*
 
         cout << "**************** Eventos Lanzados (Planificador 1) ******************" << endl;
 
@@ -646,6 +957,14 @@ int main(){
         while(x<=2000) {
             p2->run();
             x++;
+        }
+*/
+        cout << "**************** Eventos Lanzados (Planificador 3) ******************" << endl;
+
+        int y = 0;
+        while(y<=2000) {
+            p3->run();
+            y++;
         }
 
 
